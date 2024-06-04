e40df924f6a4c365b4a9c03c4d140d54c4a47ebe
mmctyeguy <tyeguy@mmc.school.nz>
2024-06-04 13:53:33 +1200

    put more of loops inside functions for simplicity + merged 08 onto base

--- 00_base_v1.py
+++ 00_base_v1.py
@@ -100,13 +100,76 @@ def menu():
     print(tabulate(sides, showindex=False, headers=sides.columns))
 
 
-# lists to hold menu items
+# checks if response is on the pizza menus
+def pizza_ordering(question, error):
+    response = input(question).lower()
+    pizza_menu = reg_menu + gourmet_menu
+
+    if response is None:
+        print(error)
+    elif response in pizza_menu:
+        pizza_order.append(response)
+        pizza_counting()
+        print("You have chosen a {}. You have {} item/s in your basket.".format(response,
+                                                                                pizza_counting.counter))
+        return response
+    elif response == "xxx":
+        return response
+    elif response in sides_menu:
+        print("Sorry, that looks like it's "
+              "from our sides menu. Please "
+              "order a pizza first.")
+    else:
+        print(error)
+
+
+# counts amount of pizzas ordered
+def pizza_counting():
+    pizza_counting.counter += 1
+    return pizza_counting.counter
+
+
+# counts amount of sides ordered
+def sides_counting():
+    sides_counting.counter += 1
+    return sides_counting.counter
+
+
+# checks if response is on the menu
+def sides_ordering(question, error):
+    response = input(question).lower()
+
+    if response is None:
+        print(error)
+    elif response == "xxx":
+        return response
+    elif response in sides_menu:
+        sides_order.append(response)
+        sides_counting()
+        print("You have chosen a {}. You have {} item/s in your basket.".format(response,
+                                                                                sides_counting.counter))
+        return response
+    else:
+        print(error)
+
+
+pizza_counting.counter = 0
+sides_counting.counter = 0
+
 reg_menu = ["cheese", "pepperoni", "hawaiian", "meatball", "vegetarian"]
 reg_price = [10, 12, 12, 15, 15]
 gourmet_menu = ["americano", "margherita", "supreme", "capricciosa", "shrimp"]
 gourmet_price = [16, 15, 19, 19, 22]
-sides_menu = ["mozzarella sticks", "L&P", "pepsi", "garlic bread", "sorbet"]
+sides_menu = ["mozzarella sticks", "l&p", "pepsi", "garlic bread", "sorbet"]
 sides_price = [14, 4, 4, 8, 9]
+
+
+pizza_order = []
+sides_order = []
+
+your_order_dict = {
+    "Pizzas": pizza_order,
+    "Sides": sides_order}
 
 # main routine begins
 
@@ -122,4 +185,39 @@ while True:
     if show_menu == "yes":
         menu()
     else:
-        print("Program continues...")
+        break
+
+# main routine here
+while True:
+    chosen_pizza = pizza_ordering("What pizza would you like?", "Please choose from our menu, or type"
+                                                                " 'xxx' to finish ordering off "
+                                                                "the Pizza menu.")
+
+    if pizza_counting.counter <= 5:
+        if chosen_pizza == "xxx":
+            if pizza_counting.counter <= 0:
+                print("Sorry, you must order at least 1 pizza.")
+            else:
+                print(pizza_order)
+                break
+
+    elif pizza_counting.counter >= 5:
+        print("Sorry, you've reached the max amount of orders")
+        print(pizza_order)
+        break
+
+while True:
+    chosen_sides = sides_ordering("What side would you like?", "Please choose from our menu or type "
+                                                               "'xxx' to finish ordering.")
+    if sides_counting.counter <= 0:
+        if chosen_sides == "xxx":
+            print("You have chosen {} sides.".format(sides_counting.counter))
+            break
+    elif sides_counting.counter >= 0:
+        if chosen_sides == "xxx":
+            print(sides_order)
+            break
+
+
+print("Here is your receipt")
+print(your_order_dict)
